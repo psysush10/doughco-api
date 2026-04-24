2a15ae507dba07fc1fee48b3105871cddb9e2552
unknown <sushanth.internal@gmail.com>
2026-04-24 23:01:34 +0530

    add: validation in controllers

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -14,19 +14,34 @@ class ProductController extends Controller
     }
     public function store(Request $request)
     {
-        $validated = $request->validate([
+        try{
+            $validated = $request->validate([
             'name' => 'required|string|max:255',
             'price' => 'required|numeric|min:1',
             'stock' => 'required|integer',
             'tag' => 'required|string',
             'description' => 'nullable|string'
-        ]);
+            ]);
 
-        $product = Product::create($validated);
+            $product = Product::create($validated);
 
-        return response()->json([
-            'success' => true,
-            'product' => $product
-        ], 201);
+            return response()->json([
+                'success' => true,
+                'product' => $product
+            ], 201);
+
+        }catch (\Illuminate\Validation\ValidationException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Validation failed',
+                'errors' => $e->errors()
+            ], 422);
+            } catch (\Exception $e) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Server error'
+                ], 500);
+            }
+        
     }
 }
